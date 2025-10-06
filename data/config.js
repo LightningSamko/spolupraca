8cd040853cf3d5bf8b0b748bbbdb55ac9b4a9b64
LightningSamko <146434381+LightningSamko@users.noreply.github.com>
2025-10-06 11:54:33 +0200

    Update config.js

--- data/config.js
+++ data/config.js
@@ -4,7 +4,7 @@
 // =====================================
 
 // ⚠️ Vlož sem svoj Discord webhook URL
-const DISCORD_WEBHOOK = "https://discord.com/api/webhooks/xxxxxxxxxxxxxxxxxxxxx";
+const DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1424694766754336820/tKhv59jNqZfuYbV60xZkH6Gnd837IUPJH6xXFeIUGxGXut-DLOpN_LZpdURBdHL0wyHg";
 
 if (typeof window !== "undefined") {
   window.DISCORD_WEBHOOK = DISCORD_WEBHOOK;
